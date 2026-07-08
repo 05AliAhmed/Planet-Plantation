0dcb44449eb230279cc76182cf033857e9ed51ff
05AliAhmed <20ali0504@gmail.com>
2026-07-08 20:42:16 +0800

    object placing in process

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,10 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
     public LayerMask placementLayerMask;
     public Camera scnCam;
     private Vector3 lastPos;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
+    {
+        
+        if(Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began) OnClicked?.Invoke();
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
+    }
+
+    // public bool IsPointerOverUI() // returns t if tap is over ui of objs n f if over 
+    //     => EventSystem.current.IsPointerOverGameObject();
+    public bool IsPointerOverUI()
+    {
+        if (Input.touchCount > 0)
+            return EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId);
+
+        return EventSystem.current.IsPointerOverGameObject();
+    }
 
     public Vector3 PosOnGrid(){
         Vector3 touchPosition = Input.mousePosition;
--- Assets/Scripts/PlacementSys.cs
+++ Assets/Scripts/PlacementSys.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,8 +8,52 @@ public class PlacementSys : MonoBehaviour
     [SerializeField] private InputManager inputManager;
     [SerializeField] private Grid grid;
 
+    [SerializeField]
+    private ObjDatabaseSO dataBase;
+    private int selectedObjectIndex = -1;
+
+    [SerializeField]
+    private GameObject gridVisualization;
+
+    private void Start()
+    {
+        StopPlacement();
+    }
+
+    public void StartPlacement(int ID){
+        StopPlacement();
+        selectedObjectIndex = dataBase.objsData.FindIndex(objInd => objInd.ID == ID);
+        if(selectedObjectIndex < 0){
+            Debug.Log($"No ID Found{ID}");
+            return;
+        }
+        gridVisualization.SetActive(true);
+        cellIndicator.SetActive(true);
+        inputManager.OnClicked += PlaceObjects;
+        inputManager.OnExit += StopPlacement;
+    }
+
+    private void PlaceObjects() // if can be added to confirm where to place the object
+    {
+        if(inputManager.IsPointerOverUI()) return;
+        Vector3 touchPos = inputManager.PosOnGrid();
+        Vector3Int cellPos = grid.WorldToCell(touchPos);
+        GameObject placeableItem = Instantiate(dataBase.objsData[selectedObjectIndex].prefab); //fetching prfab according to the objs id
+        placeableItem.transform.position = grid.CellToWorld(cellPos); 
+    }
+
+    private void StopPlacement()
+    {
+        selectedObjectIndex = -1;
+        gridVisualization.SetActive(false);
+        cellIndicator.SetActive(false);
+        inputManager.OnClicked -= PlaceObjects;
+        inputManager.OnExit -= StopPlacement;
+    }
+
     private void Update()
     {
+        if(selectedObjectIndex < 0) return;
         Vector3 touchPos = inputManager.PosOnGrid();
         Vector3Int cellPos = grid.WorldToCell(touchPos);
         objectIndicator.transform.position = touchPos;
